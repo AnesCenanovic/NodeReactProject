babcc15ed7c5dce8d2c75f77408158c66041a7d1
AnesCenanovic <acenanovic1@etf.unsa.ba>
2025-06-15 15:53:17 +0200

    forgot commit

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
 
 const el = document.getElementById("root");
 
+const store = createStore (() => reducers, {}, applyMiddleware());
+
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+    <Provider store={store}><App/></Provider>,
+);
 
