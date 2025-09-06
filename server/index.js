9a860571c99f151a943d4618c457e466fff028b3
AnesCenanovic <acenanovic1@etf.unsa.ba>
2025-09-07 01:20:34 +0200

    bugfix

--- server/index.js
+++ server/index.js
@@ -82,9 +82,9 @@ io.on('connection', socket => {
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
   app.use(express.static(path.join(__dirname, '..', 'client/build')));
-  app.get('*', (req, res) => {
+  app.get(/^\/(?!api|auth).*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
-  });
+}); 
 }
 
 const PORT = process.env.PORT || 5000
